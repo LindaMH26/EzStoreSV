fcbf3aeb41e65ea7245bd9d2bcd99168c7907597
Samuel Granados <gg.sgranados@gmail.com>
2020-07-05 17:35:21 -0600

    Email de registro

---
+++ registro.php
@@ -0,0 +1,11 @@
+<?php
+    $destino= $_POST["Email"];
+    $nombre= $_POST["Name"];  
+    mail($destino,"Registro en EzStore","Saludos " .$nombre .".\n\n
+Es un gusto que te hayas registrado en EzStore.\n
+Esperamos que encuentres todo lo que necesites\n\n
+Atte.\n
+El equipo de EzStore.\n
+Tus compras son Ez!");
+    header("Location:index.html");
+?>
